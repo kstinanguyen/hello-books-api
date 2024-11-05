97ab6a45f7fc8fc4b4a89adfa06397d191090e1f
Kristina <hngyn.kristina@gmail.com>
2024-11-04 20:30:26 -0800

    lesson 7 building an API

--- app/models/book.py
+++ app/models/book.py
@@ -6,14 +6,17 @@ class Book(db.Model):
     title: Mapped[str]
     description: Mapped[str]
 
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        return book_as_dict
     
-# books = [
-#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-# ]
+    @classmethod
+    def from_dict(cls, book_data):
+        new_book = cls(title=book_data["title"],
+                        description=book_data["description"])
+
+        return new_book
